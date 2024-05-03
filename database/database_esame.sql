879001d244410d20b41a7f403818353db2cec5f5
Riccardo Mognoni <116260265+riccardomognoni@users.noreply.github.com>
2024-05-03 12:05:12 +0200

    aggiunta database (mysql)

---
+++ database/database_esame.sql
@@ -0,0 +1,233 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 03, 2024 alle 12:03
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database_esame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `admin`
+--
+
+CREATE TABLE `admin` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cognome` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `bicicletta`
+--
+
+CREATE TABLE `bicicletta` (
+  `ID` int(11) NOT NULL,
+  `codice` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `distanza_totale` float NOT NULL,
+  `posizione_attuale` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `gps` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `RFID` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `attiva` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cognome` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `numero_tessera` int(11) NOT NULL,
+  `carta_credito` int(11) NOT NULL,
+  `IDindirizzo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `indirizzo`
+--
+
+CREATE TABLE `indirizzo` (
+  `ID` int(11) NOT NULL,
+  `via` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `citta` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `CAP` int(11) NOT NULL,
+  `provincia` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `regione` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `operazione`
+--
+
+CREATE TABLE `operazione` (
+  `ID` int(11) NOT NULL,
+  `tipo` enum('noleggio','riconsegna') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `data` date NOT NULL,
+  `orario` datetime NOT NULL,
+  `tariffa` float NOT NULL,
+  `distanza_percorsa` float NOT NULL,
+  `IDcliente` int(11) NOT NULL,
+  `IDbicicletta` int(11) NOT NULL,
+  `IDstazione` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `stazione`
+--
+
+CREATE TABLE `stazione` (
+  `ID` int(11) NOT NULL,
+  `codice` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `numero_slot` int(11) NOT NULL,
+  `IDindirizzo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indici per le tabelle `bicicletta`
+--
+ALTER TABLE `bicicletta`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `codice` (`codice`);
+
+--
+-- Indici per le tabelle `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`,`numero_tessera`),
+  ADD KEY `IDindirizzo` (`IDindirizzo`);
+
+--
+-- Indici per le tabelle `indirizzo`
+--
+ALTER TABLE `indirizzo`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `operazione`
+--
+ALTER TABLE `operazione`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `IDcliente` (`IDcliente`,`IDbicicletta`,`IDstazione`),
+  ADD KEY `IDbicicletta` (`IDbicicletta`),
+  ADD KEY `IDstazione` (`IDstazione`);
+
+--
+-- Indici per le tabelle `stazione`
+--
+ALTER TABLE `stazione`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `codice` (`codice`),
+  ADD KEY `IDindirizzo` (`IDindirizzo`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `bicicletta`
+--
+ALTER TABLE `bicicletta`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `indirizzo`
+--
+ALTER TABLE `indirizzo`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `operazione`
+--
+ALTER TABLE `operazione`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `stazione`
+--
+ALTER TABLE `stazione`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`IDindirizzo`) REFERENCES `indirizzo` (`ID`);
+
+--
+-- Limiti per la tabella `operazione`
+--
+ALTER TABLE `operazione`
+  ADD CONSTRAINT `operazione_ibfk_1` FOREIGN KEY (`IDcliente`) REFERENCES `cliente` (`ID`),
+  ADD CONSTRAINT `operazione_ibfk_2` FOREIGN KEY (`IDbicicletta`) REFERENCES `bicicletta` (`ID`),
+  ADD CONSTRAINT `operazione_ibfk_3` FOREIGN KEY (`IDstazione`) REFERENCES `stazione` (`ID`);
+
+--
+-- Limiti per la tabella `stazione`
+--
+ALTER TABLE `stazione`
+  ADD CONSTRAINT `stazione_ibfk_1` FOREIGN KEY (`IDindirizzo`) REFERENCES `indirizzo` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
